be7eae6e00d2257a64a0206cd0681bbf28e72e27
itsAman-24 <amanpan2410@gmail.com>
2025-01-03 00:15:20 +0530

    Added

--- port/src/assets/mywork_data.js
+++ port/src/assets/mywork_data.js
@@ -10,6 +10,27 @@ import project9_img from '../assets/project9.png'
 
 const mywork_data = [
     {
+        w_no:2,
+        w_name:"Web design",
+        w_img:project9_img,
+        source: "https://github.com/itsAman-24/fullstack-chat-app",
+        live: "https://fullstack-chat-app-jrdk.onrender.com/"
+    },
+    {
+        w_no:4,
+        w_name:"Web design",
+        w_img:project4_img,
+        source: "https://github.com/itsAman-24/nike",
+        live: "https://shoes-rain.netlify.app/"
+    },
+    {
+        w_no:3,
+        w_name:"Web design",
+        w_img:project1_img,
+        source: "https://github.com/itsAman-24/webdev_Projects/tree/master/TRACy-CRYPTO-APP-main",
+        live: "https://crypto-hunter.netlify.app/"
+    },
+    {
         w_no:1,
         w_name:"Web design",
         w_img:project2_img,
@@ -17,60 +38,39 @@ const mywork_data = [
         live: "https://rentthedream.netlify.app/"
     },
     {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project1_img,
-        source: "https://github.com/itsAman-24/webdev_Projects/tree/master/TRACy-CRYPTO-APP-main",
-        live: "https://crypto-hunter.netlify.app/"
-    },
-    {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project4_img,
-        source: "https://github.com/itsAman-24/nike",
-        live: "https://shoes-rain.netlify.app/"
-    },
-    {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project3_img,
-        source: "https://github.com/itsAman-24/Quiz-Game",
-        live: "https://testyourknowledge-quizgame.netlify.app"
-    },
-    {
-        w_no:5,
+        w_no:6,
         w_name:"Web design",
         w_img:project5_img,
         source: "https://github.com/itsAman-24/College_Web",
         live: "https://teal-selkie-8a2b7a.netlify.app"
     },
     {
-        w_no:6,
+        w_no:5,
         w_name:"Web design",
-        w_img:project6_img,
-        source: "https://github.com/itsAman-24/ProjectManagement",
-        live: "https://precious-alpaca-03f6aa.netlify.app/"
+        w_img:project3_img,
+        source: "https://github.com/itsAman-24/Quiz-Game",
+        live: "https://testyourknowledge-quizgame.netlify.app"
     },
     {
-        w_no:7,
+        w_no:8,
         w_name:"Web design",
         w_img:project7_img,
         source: "https://github.com/itsAman-24/Pagination_React",
         live: "https://verdant-stroopwafel-86c702.netlify.app/"
     },
     {
-        w_no:8,
+        w_no:9,
         w_name:"Web design",
         w_img:project8_img,
         source: "https://github.com/itsAman-24/ttt",
         live: "https://mellifluous-gelato-3ba4ac.netlify.app/"
     },
     {
-        w_no:9,
+        w_no:7,
         w_name:"Web design",
-        w_img:project9_img,
-        source: "https://github.com/itsAman-24/Investment",
-        live: "https://admirable-narwhal-0aa49f.netlify.app/"
+        w_img:project6_img,
+        source: "https://github.com/itsAman-24/ProjectManagement",
+        live: "https://precious-alpaca-03f6aa.netlify.app/"
     },
 ]
  
